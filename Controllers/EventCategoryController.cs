aecbf07b5be8cd54f4997da0a2953532e9b50f78
Nasser <nasser@portal365.org>
2020-12-09 05:08:08 +0300

    Add Main Controllers

---
+++ Controllers/EventCategoryController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Event.Helpers;
+using Event.Models;
+using Event.Repositories;
+
+namespace Event.Controllers
+{
+    public class EventCategoryController : ApiController
+    {
+        ApplicationDbContext db;
+        public EventCategoryController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        public IEnumerable<EventCategory> Get()
+        {
+            try
+            {
+                using (UnitOfWork unitOfWork = new UnitOfWork(db)) { return unitOfWork.EventCategory.GetAll(); }
+            }
+            catch (HttpResponseException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //this catch is for unexpected error  
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex));
+            }
+
+        }
+
+        public EventCategory Get(long id)
+        {
+            try
+            {
+                using (UnitOfWork unitOfWork = new UnitOfWork(db)) { return unitOfWork.EventCategory.Get(id); }
+            }
+            catch (HttpResponseException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //this catch is for unexpected error  
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex));
+            }
+
+        }
+
+        [HttpPost]
+        public EventCategory Add(EventCategory model)
+        {
+            try
+            {
+                if (!ModelState.IsValid) throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ModelState));
+                using (UnitOfWork unitOfWork = new UnitOfWork(db))
+                {
+                    unitOfWork.EventCategory.Add(model);
+                    unitOfWork.Complete();
+                    return model;
+
+                }
+            }
+            catch (HttpResponseException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //this catch is for unexpected error  
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex));
+            }
+
+        }
+
+
+        [HttpPut]
+        public EventCategory Update(EventCategory model)
+        {
+            try
+            {
+                if (!ModelState.IsValid) throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ModelState));
+                using (UnitOfWork unitOfWork = new UnitOfWork(db))
+                {
+                    var eventCategory = unitOfWork.EventCategory.Get(model.id);
+                    if (eventCategory == null)
+                        throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "Event Category Not Found"));
+
+                    //TODO: Update Here 
+
+                    return eventCategory;
+
+                }
+            }
+            catch (HttpResponseException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //this catch is for unexpected error  
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex));
+            }
+
+        }
+
+        [HttpDelete]
+        public EventCategory Delete(long id)
+        {
+            try
+            {
+ 
+                using (UnitOfWork unitOfWork = new UnitOfWork(db))
+                {
+                    var eventCategory = unitOfWork.EventCategory.Get(id);
+                    if (eventCategory == null)
+                        throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "Event Category Not Found"));
+
+                    //TODO: Delete  Here 
+                    unitOfWork.EventCategory.Remove(eventCategory);
+                    return eventCategory;
+                }
+            }
+            catch (HttpResponseException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //this catch is for unexpected error  
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex));
+            }
+
+        }
+
+    }
+}
---
+++ Controllers/EventController.cs
@@ -0,0 +1,202 @@
+﻿using Event.Models;
+using Event.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Event.Controllers
+{
+    public class EventController : ApiController
+    {
+         ApplicationDbContext db;
+        public EventController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        public IEnumerable<Models.Event> Get()
+        {
+            try
+            {
+                using (UnitOfWork unitOfWork = new UnitOfWork(db)) { return unitOfWork.Event.GetAll(); }
+            }
+            catch (HttpResponseException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //this catch is for unexpected error  
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex));
+            }
+
+        }
+
+        public Models.Event Get(long id)
+        {
+            try
+            {
+                using (UnitOfWork unitOfWork = new UnitOfWork(db)) { return unitOfWork.Event.Get(id); }
+            }
+            catch (HttpResponseException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //this catch is for unexpected error  
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex));
+            }
+
+        }
+
+        [HttpPost]
+        public Models.Event Add(Models.Event model)
+        {
+            try
+            {
+                if (!ModelState.IsValid) throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ModelState));
+                using (UnitOfWork unitOfWork = new UnitOfWork(db))
+                {
+                    unitOfWork.Event.Add(model);
+                    unitOfWork.Complete();
+                    return model;
+
+                }
+            }
+            catch (HttpResponseException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //this catch is for unexpected error  
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex));
+            }
+
+        }
+
+
+        [HttpPut]
+        public Models.Event Update(Models.Event model)
+        {
+            try
+            {
+                if (!ModelState.IsValid) throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ModelState));
+                using (UnitOfWork unitOfWork = new UnitOfWork(db))
+                {
+                    var @event = unitOfWork.Event.Get(model.id);
+                    if (@event == null)
+                        throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "Event Not Found"));
+
+                    //TODO: Update Here 
+
+                    return @event;
+
+                }
+            }
+            catch (HttpResponseException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //this catch is for unexpected error  
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex));
+            }
+
+        }
+
+        [HttpDelete]
+        public Models.Event Delete(long id)
+        {
+            try
+            {
+
+                using (UnitOfWork unitOfWork = new UnitOfWork(db))
+                {
+                    var @event = unitOfWork.Event.Get(id);
+                    if (@event == null)
+                        throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "Event Not Found"));
+
+                    //TODO: Delete  Here 
+                    unitOfWork.Event.Remove(@event);
+                    return @event;
+                }
+            }
+            catch (HttpResponseException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //this catch is for unexpected error  
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex));
+            }
+
+        }
+
+
+        [HttpPut]
+        [Route("event/:id/participate")]
+        public Models.Event RegistarNewParticipant(long id,UserParticipantsEvent userParticipant)
+        {
+            try
+            {
+                if (!ModelState.IsValid) throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ModelState));
+
+                using (UnitOfWork unitOfWork = new UnitOfWork(db))
+                {
+                    var @event = unitOfWork.Event.Get(id);
+                    if (@event == null)
+                        throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "Event Not Found"));
+
+                    unitOfWork.UserParticipantsEvent.Add(userParticipant);
+                    return @event;
+                }
+            }
+            catch (HttpResponseException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //this catch is for unexpected error  
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex));
+            }
+
+        }
+
+        [HttpPut]
+        [Route("event/:id/unparticipate")]
+        public Models.UserParticipantsEvent Unparticipant(long id,string userId)
+        {
+            try
+            {
+ 
+                using (UnitOfWork unitOfWork = new UnitOfWork(db))
+                {
+                    var userParticipant = unitOfWork.UserParticipantsEvent.Find(e => e.eventId == id && e.userId == userId).FirstOrDefault();
+                    if (userParticipant == null)
+                        throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "Not Register before"));
+
+                    unitOfWork.UserParticipantsEvent.Remove(userParticipant);
+                    return userParticipant;
+                }
+            }
+            catch (HttpResponseException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //this catch is for unexpected error  
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex));
+            }
+
+        }
+    }
+}
